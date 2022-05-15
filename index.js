8a0122ff7c44d6d4e6f7f2878ef9e41c60e06f3d
Lincoln Martinez <lincoljosue7@gmail.com>
2022-05-15 18:44:37 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 8080; // Route to be tested
+app.get("/", (req, res) => {
+  return res.status(200).json({ name: "Lincoln Martinez" });
+}); // Application running on the door
+let server = app.listen(port, () => {
+  console.log(`Application running on ${port}`);
+});
+module.exports = server;
---
+++ tests/index.test.js
@@ -0,0 +1,18 @@
+const request = require("supertest");
+const server = require("../index");
+
+describe("Get Endpoints", () => {
+  it("Get", async () => {
+    const res = await request(server).get("/").send({
+      userId: 1,
+      title: "test is cool",
+    });
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toHaveProperty("name");
+  });
+});
+
+afterAll(async () => {
+  // close server conection
+  server.close();
+});
